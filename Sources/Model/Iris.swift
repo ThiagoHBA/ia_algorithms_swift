b65291a8de56b90d551a8e3b7678c1436c6aa068
ThiagoHBA <thiagohhab@gmail.com>
2023-05-08 11:56:27 -0300

    generalizating code for all algorithms

---
+++ Sources/Model/Iris.swift
@@ -0,0 +1,39 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thiago Henrique on 08/05/23.
+//
+
+import Foundation
+
+struct Iris: Decodable {
+    var id: Int
+    var sepalLenght: Double
+    var sepalWidth: Double
+    var petalLenght: Double
+    var petalWidth: Double
+    let species: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id = "Id"
+        case sepalLenght = "SepalLengthCm"
+        case sepalWidth = "SepalWidthCm"
+        case petalLenght = "PetalLengthCm"
+        case petalWidth = "PetalWidthCm"
+        case species = "Species"
+    }
+    
+    static func loadData() -> [Iris] {
+        guard let url = Bundle.module.url(forResource: "Iris", withExtension: "json") else { return [] }
+        do {
+            let data = try Data(contentsOf: url)
+            let json = try JSONDecoder().decode([Iris].self, from: data)
+            return json
+        }
+        catch {
+            print(error)
+        }
+        return []
+    }
+}
---
+++ Sources/main.swift
@@ -0,0 +1,4 @@
+import Foundation
+
+let data = Iris.loadData()
+print(data)
