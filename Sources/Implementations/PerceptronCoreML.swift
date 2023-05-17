4137b8d8ea3cc1f3f4dabc7bd6b16c443242de9c
ThiagoHBA <thiagohhab@gmail.com>
2023-05-17 09:03:43 -0300

    creating a coreml adapter to the binary classifier

---
+++ Sources/Implementations/PerceptronCoreML.swift
@@ -0,0 +1,69 @@
+//
+//  File.swift
+//  
+//
+//  Created by Thiago Henrique on 16/05/23.
+//
+
+import Foundation
+import CoreML
+
+/// CoreMLAdapter class used to adapt a BinaryClassifier model to a CoreML custom model.
+@objc(Perceptron)
+class CoreMLAdapter: NSObject, MLCustomModel {
+    var classifier: BinaryClassifier
+    
+    required init(modelDescription: MLModelDescription, parameters: [String : Any]) throws {
+        self.classifier = parameters["classifier"] as! any BinaryClassifier
+        super.init()
+    }
+    
+    func prediction(from input: MLFeatureProvider, options: MLPredictionOptions) throws -> MLFeatureProvider {
+        guard let inputInstance = input.featureValue(for: "vector")?.multiArrayValue else { return IrisOutput(output: 0)}
+        let vector = SIMD4<Double>(
+            Double(truncating: inputInstance[0]),
+            Double(truncating: inputInstance[1]),
+            Double(truncating: inputInstance[2]),
+            Double(truncating: inputInstance[3])
+        )
+        return IrisOutput(output: classifier.predict(vector))
+    }
+}
+
+/// Representation of expected features who are used to input values on prediction
+final class IrisInput: MLFeatureProvider {
+    let vector: SIMD4<Double>
+    
+    init(vector: SIMD4<Double>) { self.vector = vector }
+    
+    var featureNames: Set<String> { get { return ["vector"] } }
+    
+    func featureValue(for featureName: String) -> MLFeatureValue? {
+        if featureName == "vector" {
+            if #available(macOS 12.0, *) {
+                return MLFeatureValue(shapedArray: [vector.x, vector.y, vector.z, vector.w])
+            }
+        }
+        return nil
+    }
+}
+
+/// Representation of expected features who are used when the classifier return a prediction
+final class IrisOutput: MLFeatureProvider {
+    let output: Int
+    
+    init(output: Int) {
+        self.output = output
+    }
+    
+    var featureNames: Set<String> { get { return ["output"] } }
+    
+    func featureValue(for featureName: String) -> MLFeatureValue? {
+        if featureName == "output" {
+            return MLFeatureValue(int64: Int64(output))
+        }
+        return nil
+    }
+}
+
+
--- Sources/main.swift
+++ Sources/main.swift
@@ -1,4 +1,5 @@
 import Foundation
+import CoreML
 
 let dataset = DataUtility.splitDataset(Iris.loadData(), trainingFactor: 0.7)
 print("Training dataset lenght: \(dataset.0.count)")
@@ -15,7 +16,22 @@ print("Test dataset lenght: \(dataset.1.count)")
 //print("Percent: \(Validator.validateClassifier(classifier: dmc, testDataset: dataset.1)) %\n")
 
 // MARK: - Percepton
-print("Perceptron: ")
-let expectedSpecie: Specie = .IrisVirginica
-let perceptron = Perceptron(dataset: dataset, expectedSpecie: expectedSpecie, ephocs: 200)
-print("Percent: \(Validator.validateClassifier(classifier: perceptron, testDataset: dataset.1, expectedClass: expectedSpecie)) %\n")
+//print("Perceptron: ")
+//let expectedSpecie: Specie = .IrisVersicolor
+//let perceptron = Perceptron(dataset: dataset, expectedSpecie: expectedSpecie, ephocs: 200)
+//print("Percent: \(Validator.validateClassifier(classifier: perceptron, testDataset: dataset.1, expectedClass: expectedSpecie)) %\n")
+
+// MARK: - Percepton CoreML
+let perceptron = Perceptron(dataset: dataset, expectedSpecie: .IrisSetosa)
+perceptron.train()
+
+let modelDescription = MLModelDescription()
+let coreMLPerceptron = try CoreMLAdapter(modelDescription: modelDescription, parameters: ["classifier": perceptron])
+
+for testInstance in dataset.1 {
+    let expected = testInstance.species == Specie.IrisSetosa
+    let result = try coreMLPerceptron.prediction(from: IrisInput(vector: testInstance.vector!), options: MLPredictionOptions())
+    print("Expected: \(expected) ; Predicted: \(result.featureValue(for: "output")!)")
+}
+
+
